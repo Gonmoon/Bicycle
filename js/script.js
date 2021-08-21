9d8b496c725439ed51859a4c3f37de07470d88d9
Gonmoo <86917638+Gonmoon@users.noreply.github.com>
2021-08-21 20:27:04 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,30 @@
+$(function(){
+	// Adaptiv
+	if ($(document).width() <= 768){
+		$('.nav').hide();
+	};
+	// Header
+	$('.icon').click(function(){
+		$(this).toggleClass('icon_open');
+		$('.nav').slideToggle(300);
+	});
+	// Slider
+	$('.news__body-slider').slick({
+		arrows:false,
+		dots:true,
+		autoplay:true,
+	});
+	// Image
+	if ($(document).width() <= 768){
+		$('#hover').addClass('image__hover');
+	} else {
+		$('#hover').hide();
+		$('.image').mouseover(function(){
+			$('#hover').show();
+			$('#hover').addClass('image__hover');
+		});
+		$('.image').mouseleave(function(){
+			$('#hover').hide();
+		});
+	};
+});
